7406f137058c155a47a67c144f4e608bc0086733
adamsyy <adamoommenjacob@gmail.com>
2022-01-16 01:42:34 +0530

    currents

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const request=require('request')
 const url='http://api.weatherstack.com/current?access_key=28aafc29d8dd91aeb4df864877a1021b&query=37.865,-122.865'
 request({url:url,json:true}, function (error, response,body) {
 
-  console.log(response.body.current); // Print the HTML for the Google homepage.
-});
+  console.log(response.body.current.weather_descriptions[0]+' weather'); // Print the HTML for the Google homepage.
+}); 
