56e8dd58af5db2bec079c630fe8c0e54e48dfa17
adamsyy <adamoommenjacob@gmail.com>
2022-01-18 01:29:39 +0530

    node_modules

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const request=require('request')
 const url='http://api.weatherstack.com/current?access_key=28aafc29d8dd91aeb4df864877a1021b&query=37.865,-122.865'
 request({url:url,json:true}, (error, response,body)=>{
 
-
 }); 
 try{
     request({url:'https://api.mapbox.com/geocoding/v5/mapbox.places/smsmsmsmsm.json?access_token=pk.eyJ1IjoiYWRhbXJ1YmlrIiwiYSI6ImNreWc5eGl6eTBjNTkyb3FuODZoa3hndmIifQ.I2kv0aaBz4vhLeNuYG1OnA&limit=1',json:true},(request,response)=>{
@@ -14,4 +13,5 @@ console.log('latitude:'+latitude+'\nlongitude:'+longitude)
 }catch(e){
     console.log(e)
 }
+
 
