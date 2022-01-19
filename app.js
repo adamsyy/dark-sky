d2c847f7b910214191cbf861f83500d21b6f385f
adamsyy <adamoommenjacob@gmail.com>
2022-01-19 14:30:16 +0530

    almost set

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-forecast(10.02859,76.328716,(error,body)=>{
-    console.log(body)
-    })
+
+
+geocode('Thiruvalla', (error, data) => {
+    if (error) {
+        console.log('some error')
+    }
+  
+    forecast(data.latitude,data.longitude,(error,body)=>{
+        console.log(body)
+        })
+})
+
+
+
      
 
 
-
-// geocode('model engineering college', (error, data) => {
-//     if (error) {
-//         console.log('some error')
-//     }
-//     console.log(data.latitude)
-//     console.log(data.longitude)
-//     console.log(data.location)
-// })
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=28aafc29d8dd91aeb4df864877a1021b&query=37.865,-122.865'
+    const url = 'http://api.weatherstack.com/current?access_key=28aafc29d8dd91aeb4df864877a1021b&query='+latitude+','+longitude
 request({url:url,json:true},(error,response,body)=>{
 callback(undefined,body)
 })
