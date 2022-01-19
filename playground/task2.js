a8f37f28ac1757dcc2579f019ece62b4731b202a
adamsyy <adamoommenjacob@gmail.com>
2022-01-19 13:47:05 +0530

    task2 update

--- playground/task2.js
+++ playground/task2.js
@@ -1,8 +1,8 @@
 const add=((a,b,callback)=>{
     setTimeout(()=>{
-        const sum=a+b;
-        callback(sum)
-    })
+     
+        callback(a+b)
+    },2000)
 })
 add(3,4,(sum)=>{
     console.log(sum)
