464dac999c748cb1f31d8bef7fdf5bd96e31c1bf
adamsyy <adamoommenjacob@gmail.com>
2022-01-19 12:46:43 +0530

    error handling

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const url='http://api.weatherstack.com/current?access_key=28aafc29d8dd91aeb4df86
 request({url:url,json:true}, (error, response,body)=>{
 
 }); 
-try{
-    request({url:'https://api.mapbox.com/geocoding/v5/mapbox.places/smsmsmsmsm.json?access_token=pk.eyJ1IjoiYWRhbXJ1YmlrIiwiYSI6ImNreWc5eGl6eTBjNTkyb3FuODZoa3hndmIifQ.I2kv0aaBz4vhLeNuYG1OnA&limit=1',json:true},(request,response)=>{
-const latitude=response.body.features[0].center[1] 
-const longitude=response.body.features[0].center[0]  
-console.log('latitude:'+latitude+'\nlongitude:'+longitude)
+
+    request({url:'https://api.mapbox.com/geocoding/v5/mapbox.places/12wat.json?access_token=pk.eyJ1IjoiYWRhbXJ1YmlrIiwiYSI6ImNreWc5eGl6eTBjNTkyb3FuODZoa3hndmIifQ.I2kv0aaBz4vhLeNuYG1OnA&limit=1',json:true},(error,response,body)=>{
+if(error){console.log('no internet')}
+else if(response.body.features.length==0){console.log('unable to locate the area')}
+else{  const latitude=response.body.features[0].center[1] 
+    const longitude=response.body.features[0].center[0]  
+    console.log('latitude:'+latitude+'\nlongitude:'+longitude)}
+  
 })
-}catch(e){
-    console.log(e)
-}
 
 
