383b025dbc93becbdf065d46da4fb906c9e64f4a
adamsyy <adamoommenjacob@gmail.com>
2022-01-20 18:39:54 +0530

    argv[2] initiated

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 
+var arguments = process.argv;
+if (arguments[2]) {
+    arguments[2]=arguments[2].toString()
+    geocode(arguments[2], (error, data) => {
+        if (error) {
+            console.log('some error')
+        }
 
-geocode('Thiruvalla', (error, data) => {
-    if (error) {
-        console.log('some error')
-    }
-  
-    forecast(data.latitude,data.longitude,(error,body)=>{
-        console.log(body)
+        forecast(data.latitude, data.longitude, (error, forecastdata) => {
+            console.log(forecastdata)
+            console.log(data.location)
         })
-})
 
-
-
-     
-
-
+    })
+}
