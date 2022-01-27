5955f145edadc12fe87973be77e08d02b268a95b
adamsyy <adamoommenjacob@gmail.com>
2022-01-28 00:48:45 +0530

    help added

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,31 @@
 const path = require('path')
-const hbs=require('hbs')
+const hbs = require('hbs')
 const express = require('express')
 const app = express()
-app.set('view engine','hbs')
+const viwespath = path.join(__dirname, '../view')
 const pubdirpath = path.join(__dirname, '../public');
+app.set('view engine', 'hbs')
+app.set('views', viwespath)
+
 app.use(express.static(pubdirpath))
- 
-app.get('',(req,res)=>{
-    res.render('index')
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'weather app',
+        name: 'adamsy'
+    })
 })
 
 app.get('/about', (req, res) => {
-
-})
-app.get('/weather', (req, res) => {
-    res.send({
-        'weather': 'weather'
+    res.render('about', {
+        title: 'about me',
+        name: 'Adamsy'
     })
+})
+app.get('/help', (req, res) => {
 
+    res.render('help')
 })
 
 app.listen(3000, () => {
