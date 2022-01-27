2b97196ad4c4481c3068726330c7fd1ddfd24983
adamsyy <adamoommenjacob@gmail.com>
2022-01-27 23:27:55 +0530

    hbs set

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,15 @@
 const path = require('path')
+const hbs=require('hbs')
 const express = require('express')
 const app = express()
+app.set('view engine','hbs')
 const pubdirpath = path.join(__dirname, '../public');
-
-
 app.use(express.static(pubdirpath))
  
+app.get('',(req,res)=>{
+    res.render('index')
+})
+
 app.get('/about', (req, res) => {
 
 })
