7b844f7ff90871b1fa269f49065abe5db660e779
adamsyy <adamoommenjacob@gmail.com>
2022-01-27 20:50:24 +0530

    weather

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,5 +4,16 @@ const app=express()
 app.get('',(req,res)=>{
     res.send('eh working ahne')
 })
+app.get('/about',(req,res)=>{
+    res.send('<h1>About</h1>')
+})
+app.get('/weather',(req,res)=>{
+    res.send({
+        'weather':'weather'
+    })
 
-app.listen(3000)
+})
+
+app.listen(3000,()=>{
+    console.log('server working')
+})
