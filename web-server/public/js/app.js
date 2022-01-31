4c8085f062852957bb8c3c872babfd333c7099cf
adamsyy <adamoommenjacob@gmail.com>
2022-01-31 23:03:15 +0530

    added event listetener

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,20 @@
 console.log('load aaye')
+
+fetch('http://localhost:3000/weather?address=thiruvalla').then((response)=>{
+
+    response.json().then((data)=>{
+        if(data.error){
+            console.log('full error')
+        }
+        else{
+            console.log(data)
+        }
+        
+    })
+})  
+
+const weatherform=document.querySelector('form')
+weatherform.addEventListener('submit',(e)=>{
+    e.preventDefault()
+console.log('settesth')
+})
